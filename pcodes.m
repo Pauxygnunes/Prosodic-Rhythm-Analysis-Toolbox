7b3c21808b6e16adfc107534307e3ce9e93b039e
Pauxy Gentil-Nunes <30673056+Pauxygnunes@users.noreply.github.com>
2025-01-28 21:26:19 -0300

    Update pcodes.m

--- pcodes.m
+++ pcodes.m
@@ -17,8 +17,8 @@ function pcodes = pcodes(n)
 % 
 % Created in July 2023 under MATLAB 2022 (Mac OS)
 %
-% © Part of Prosodic Rhythms Analysis Toolbox - PRA Toolbox,
-% Copyright © 2023, Pauxy Gentil Nunes Filho, Filipe de Matos Rocha
+% ï¿½ Part of Prosodic Rhythms Analysis Toolbox - PRA Toolbox,
+% Copyright ï¿½ 2023, Pauxy Gentil Nunes Filho, Filipe de Matos Rocha
 % PArtiMus, and MusMat Research Groups - PPGM-UFRJ
 % See License.txt
 % ==========
